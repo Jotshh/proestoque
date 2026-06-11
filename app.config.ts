3069dd4b72281396ced0b9f16b78854aeaa434e8
Josiel <josiephelipel265@gmail.com>
2026-06-11 16:00:37 -0400

    chore: point API to production Railway URL

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,12 @@ export default {
 
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:3333/api",
+      eas: {
+        projectId: "9afc1f31-ae1d-4cd9-9d80-62807c331fe8",
+      },
     },
+
+    plugins: [["expo-notifications" ]],
+    
   },
 };
